8635312a94d3ebcd545fe3608b376b96bd2d66c0
Joel Lamata <jlamata88@gmail.com>
2023-05-17 12:30:38 +0200

    arreglado cambio de imagenes en los levels

--- src/Database.vue
+++ src/Database.vue
@@ -32,34 +32,64 @@ const defaultImagesBelleza = [
     "https://s1.ppllstatics.com/diariovasco/www/multimedia/202003/12/media/cortadas/interior_makeup_624-kqqH-U100483645388dmC-1248x770@RC.jpg",
     "https://mejorconsalud.as.com/wp-content/uploads/2016/02/tratamiento-belleza.jpg",
     "https://i.blogs.es/76d175/318208931_700294888142342_818714537198339726_n/1366_2000.jpeg",
-    "https://www.hola.com/imagenes/belleza/actualidad/20210514189532/concurso-productos-cosmetica-perfumes-maquillaje/0-951-916/concurso-belleza-e.jpg"
+    "https://www.hola.com/imagenes/belleza/actualidad/20210514189532/concurso-productos-cosmetica-perfumes-maquillaje/0-951-916/concurso-belleza-e.jpg",
 ]
 const coloresBelleza = "https://clinicaarquero.es/wp-content/uploads/que-es-la-belleza_1920.jpg"
 const floresBelleza = "https://e00-elmundo.uecdn.es/assets/multimedia/imagenes/2023/04/25/16824261489972.jpg"
 const cuidadosBelleza = "https://img.freepik.com/foto-gratis/mujer-hermosa-joven-flores-junto-cara_186202-5624.jpg"
 const maquillajeBelleza = "https://proyectoscio.ucv.es/wp-content/uploads/2022/07/belleza-600x450.jpg"
 
-const Levels = {1: 
-                    { 
-                        "defaultImages": defaultImagesBelleza,
-                        "replacementImages": [
-                            {"image": coloresBelleza, "label": "Colores", "replacementIndex": 0}, 
-                            {"image": floresBelleza, "label": "Flores", "replacementIndex": 3}, 
-                            {"image": cuidadosBelleza, "label": "Cuidados", "replacementIndex": 1}, 
-                            {"image": maquillajeBelleza, "label": "Maquillaje", "replacementIndex": 2}
-                        ],
-                        "poliForm": [octagon, hexagon],
-                        "placeholder": "Belleza",
-                        "poliNum": 3,
-                        "badPoliNum": 1,
-                        "poliText": ["Colores", "Flores", "Cuidados"],
-                        "badPoliText": ["Maquillaje"],
-                        "points": 0,
-                        "levelDescription": {"header": "hola header", "body": "hola body", "footer": "hola footer"}
-                    },
-                }
+// Juguetes para niñas
+const defaultImagesJuguetesParaNiñas = [
+    "https://nh1.revoou.com/guiasdecompra/wp-content/uploads/2022/12/71aWljsnUkL-1024x1024.jpg",
+    "https://cholloblog.com/wp-content/uploads/2022/08/maquillaje-520x450.jpg",
+    "https://images.ecestaticos.com/xFTkhy7PODdIsxMgMzdFFKCxqfM=/0x0:640x361/1440x810/filters:fill(white):format(jpg)/f.elconfidencial.com%2Foriginal%2F73d%2F33f%2F35e%2F73d33f35e8b66ab404588b8a1d500879.jpg",
+    "https://www.pixologic.es/wp-content/uploads/2022/10/juguetes-para-ninas-728lfc.jpg",
+    "https://www.tododisca.com/wp-content/uploads/2022/12/Ninos-jugando-con-los-juguetes-que-recomienda-la-OCU-1140x703.jpg",
+    "https://m.media-amazon.com/images/I/71s2vjqueSL._AC._SR360,460.jpg",
+    "https://img.freepik.com/vector-gratis/juego-juguetes-ninos_74855-7019.jpg",
+    "https://img.remediosdigitales.com/e50759/71mfb4dmbrs._ac_sl1000_/450_1000.jpeg"
+]
+const Levels = {
+    1:
+    {
+        "defaultImages": defaultImagesBelleza,
+        "replacementImages": [
+            { "image": coloresBelleza, "label": "Colores", "replacementIndex": 0 },
+            { "image": floresBelleza, "label": "Flores", "replacementIndex": 3 },
+            { "image": cuidadosBelleza, "label": "Cuidados", "replacementIndex": 1 },
+            { "image": maquillajeBelleza, "label": "Maquillaje", "replacementIndex": 2 }
+        ],
+        "poliForm": [octagon, hexagon],
+        "placeholder": "Belleza",
+        "poliNum": 3,
+        "badPoliNum": 1,
+        "poliText": ["Colores", "Flores", "Cuidados"],
+        "badPoliText": ["Maquillaje"],
+        "points": 0,
+        "levelDescription": { "header": "hola header", "body": "hola body", "footer": "hola footer" }
+    },
+    2:
+    {
+        "defaultImages": defaultImagesJuguetesParaNiñas,
+        "replacementImages": [
+            { "image": coloresBelleza, "label": "Colores", "replacementIndex": 0 },
+            { "image": floresBelleza, "label": "Flores", "replacementIndex": 3 },
+            { "image": cuidadosBelleza, "label": "Cuidados", "replacementIndex": 1 },
+            { "image": maquillajeBelleza, "label": "Maquillaje", "replacementIndex": 2 }
+        ],
+        "poliForm": [octagon, hexagon],
+        "placeholder": "Belleza",
+        "poliNum": 3,
+        "badPoliNum": 1,
+        "poliText": ["Colores", "Flores", "Cuidados"],
+        "badPoliText": ["Maquillaje"],
+        "points": 0,
+        "levelDescription": { "header": "hola header", "body": "hola body", "footer": "hola footer" }
+    },
+}
 
-export default{
+export default {
     Levels
 }
 </script>
--- src/Level.vue
+++ src/Level.vue
@@ -1,5 +1,5 @@
 <script setup>
-import { ref } from 'vue'
+import { ref, nextTick, onUpdated, onMounted, watch } from 'vue'
 import poligon from './Poligons.vue'
 import Database from './Database.vue'
 import Modal from './Modal.vue'
@@ -8,7 +8,7 @@ import _ from 'lodash'
 const props = defineProps(['defaultImages', 'replacementImages', 'placeholder', 'poliForm', 'badPoliForm', 'poliNum', 'badPoliNum', 'poliText', 'badPoliText', 'levelNum', 'time'])
 
 const defImgs = _.cloneDeep(props.defaultImages)
-var imagesToShow = _.cloneDeep(props.defaultImages);
+var imagesToShow = ref(props.defaultImages);
 let correctPoligons = [];
 
 function setPoints() {
@@ -30,7 +30,7 @@ function isOnSquare(e) { //Cambiar nombre?
         poligonPosition.bottom <= basuraPosition.bottom &&
         poligonPosition.top >= basuraPosition.top) {
         changeImage(label, false);
-        if(props.badPoliText.includes(label)){
+        if (props.badPoliText.includes(label)) {
             correctPoligons.push(label);
         }
         console.log(correctPoligons)
@@ -38,7 +38,7 @@ function isOnSquare(e) { //Cambiar nombre?
     else {
         changeImage(label, true);
         const idx = correctPoligons.indexOf(label);
-        if(idx != -1){
+        if (idx != -1) {
             correctPoligons.splice(idx, 1);
         }
         console.log(correctPoligons)
@@ -47,9 +47,9 @@ function isOnSquare(e) { //Cambiar nombre?
 
 function changeImage(label, toDefault) {
     const imageObject = props.replacementImages.find(item => item.label === label);
-    if(imageObject){
+    if (imageObject) {
         const imageIndex = imageObject.replacementIndex;
-        if(toDefault) {
+        if (toDefault) {
             imagesToShow[imageIndex] = _.cloneDeep(defImgs[imageIndex]); // Change to default
         } else {
             imagesToShow[imageIndex] = _.cloneDeep(imageObject.image);   // Change to replacement
@@ -57,16 +57,20 @@ function changeImage(label, toDefault) {
     }
 }
 
-function resetLevel() {
+async function resetLevel() {
     // Reset poligons position
     for (let i = 0; i < props.poliNum + props.badPoliNum; i++) {
         this.$refs.poligons.children[i].style["top"] = "0"
         this.$refs.poligons.children[i].style["left"] = "0"
     }
     // Reset image
-    imagesToShow = props.defaultImages;
+    // imagesToShow = props.defaultImages;
     correctPoligons = [];
 }
+
+watch(() => props.defaultImages, (newVal) => {
+    imagesToShow = newVal;
+})
 
 defineExpose({ setPoints, resetLevel })
 </script>
@@ -75,14 +79,15 @@ defineExpose({ setPoints, resetLevel })
     <div class="level">
         <div class="imgbox">
             <input type="search" :placeholder="props.placeholder" class="search_input" disabled>
-            <section class="images" :style="{ 'column-count': 2}">
-                <img v-for="i in props.defaultImages.length" :src="imagesToShow[i - 1]">
-                <!-- <img v-for="label in touchingPoligons" :src="getImage(label)"> -->
-            </section>
+            <div class="images-container" :class="{ 'two-columns': imagesToShow.length === 4 }">
+                <img v-for="i in imagesToShow.length" :src="imagesToShow[i - 1]">
+            </div>
         </div>
         <div class="poligons" ref="poligons">
-            <poligon v-for="i in props.poliNum" :poliForm="props.poliForm" :text="props.poliText[i - 1]" @click="isOnSquare" :id="'poli-' + i"/>
-            <poligon v-for="i in props.badPoliNum" :poliForm="props.badPoliForm" :text="props.badPoliText[i - 1]" @click="isOnSquare" :id="'badpoli-' + i"/>
+            <poligon v-for="i in props.poliNum" :poliForm="props.poliForm" :text="props.poliText[i - 1]" @click="isOnSquare"
+                :id="'poli-' + i" />
+            <poligon v-for="i in props.badPoliNum" :poliForm="props.badPoliForm" :text="props.badPoliText[i - 1]"
+                @click="isOnSquare" :id="'badpoli-' + i" />
             <div class="basura">
                 <img src="https://cdn.icon-icons.com/icons2/1791/PNG/512/trashcan1_114647.png">
             </div>
@@ -93,47 +98,50 @@ defineExpose({ setPoints, resetLevel })
 <style>
 .level {
     display: grid;
-    grid-template-columns: repeat(2, 1fr);
+    grid-auto-columns: minmax(0, 1fr);
+    grid-auto-flow: column;
     grid-template-rows: 1fr;
-    grid-column-gap: 0px;
+    grid-column-gap: 2px;
     grid-row-gap: 0px;
 }
 
-.images {
-    line-height: 0;
-    column-gap: 5px;
-    margin: 5px;
-    max-height: auto;
+.imbox {
+    width: 50vh;
 }
 
-.images img {
-    margin-top: 5px;
-    border-radius: 16px;
-    width: 50vh;
-    height: 35vh;
+.images-container {
+    display: grid;
+    grid-template-columns: repeat(3, 1fr);
+    grid-template-rows: repeat(2, 1fr);
+    max-width: 100%;
+    max-height: 70vh;
+}
+
+.images-container.two-columns {
+    grid-template-columns: repeat(2, minmax(0, 1fr));
+}
+
+.images-container img {
+    width: 100%;
+    height: 100%;
     object-fit: cover;
+    border-radius: 16px;
+    margin-top: 5px;
 }
 
 .search_input {
     width: 100%;
     padding: 12px 24px;
-
     background-color: whitesmoke;
-    transition: transform 250ms ease-in-out;
     font-size: 14px;
     line-height: 18px;
-
     color: #575756;
-
     background-image: url("data:image/svg+xml;charset=utf8,%3Csvg xmlns='http://www.w3.org/2000/svg' width='24' height='24' viewBox='0 0 24 24'%3E%3Cpath d='M15.5 14h-.79l-.28-.27C15.41 12.59 16 11.11 16 9.5 16 5.91 13.09 3 9.5 3S3 5.91 3 9.5 5.91 16 9.5 16c1.61 0 3.09-.59 4.23-1.57l.27.28v.79l5 4.99L20.49 19l-4.99-5zm-6 0C7.01 14 5 11.99 5 9.5S7.01 5 9.5 5 14 7.01 14 9.5 11.99 14 9.5 14z'/%3E%3Cpath d='M0 0h24v24H0z' fill='none'/%3E%3C/svg%3E");
     background-repeat: no-repeat;
     background-size: 18px 18px;
     background-position: 95% center;
     border-radius: 50px;
     border: 1px solid #575756;
-    transition: all 250ms ease-in-out;
-    backface-visibility: hidden;
-    transform-style: preserve-3d;
 }
 
 .poligons {
@@ -152,7 +160,7 @@ defineExpose({ setPoints, resetLevel })
 }
 
 .basura img {
-    height: 10vh;
+    height: 80%;
     position: absolute;
     top: 0;
     bottom: 0;
