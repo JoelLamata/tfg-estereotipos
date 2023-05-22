2a8e096618075fb00334b2f0cd4264cda7017eec
Joel Lamata <jlamata88@gmail.com>
2023-05-22 18:48:34 +0200

    bootstrap installed

--- src/main.js
+++ src/main.js
@@ -2,6 +2,12 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import drag from "v-drag"
 
+// Import our custom CSS
+import './scss/styles.scss'
+
+// Import all of Bootstrap's JS
+import * as bootstrap from 'bootstrap'
+
 const app = createApp(App);
 
 app.use(drag, {
--- vite.config.js
+++ vite.config.js
@@ -3,12 +3,15 @@ import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
+const path = require('path')
+
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
   base: '/tfg-estereotipos/',
   resolve: {
     alias: {
+      '~bootstrap': path.resolve(__dirname, 'node_modules/bootstrap'),
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
   }
